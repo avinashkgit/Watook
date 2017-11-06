4580c40d30be3d9cede537209996d1caae531409
avinashkgit <avinash.kumar@extrapreneursindia.com>
2017-11-06 10:33:22 +0530

    fixes

--- app/src/main/java/com/watook/util/DividerItemDecorator.java
+++ app/src/main/java/com/watook/util/DividerItemDecorator.java
@@ -22,7 +22,7 @@ public class DividerItemDecorator extends RecyclerView.ItemDecoration {
         int dividerRight = parent.getWidth() - parent.getPaddingRight();
 
         int childCount = parent.getChildCount();
-        for (int i = 0; i <= childCount - 3; i++) {
+        for (int i = 0; i <= childCount - 2; i++) {
             View child = parent.getChildAt(i);
 
             RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
